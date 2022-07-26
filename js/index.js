5f817b0b52462d2f8160d7a7c58dbbced4579964
Miqueas Gutierrez <miqueasgr@gmail.com>
2022-07-26 14:03:50 -0300

    agregando funciones

--- js/index.js
+++ js/index.js
@@ -6,9 +6,9 @@ nombre = prompt("Como es tu nombre?")
 
 apellido = prompt("Cual es tu apellido?")
 
-let nombreCompleto = nombre+espacio+apellido
+let nombreCompleto = nombre + espacio + apellido
 
-alert('Holaa '+nombreCompleto+' es un placer tenerte por aqui!')
+alert('Holaa ' + nombreCompleto + ' es un placer tenerte por aqui!')
 
 
 let saludos = parseInt(prompt("Estamos tan felices de tenerte que te saludaremos las veces que quieras! Indica la cantidad de veces que quieres un saludo de nuetra parte"))
@@ -41,48 +41,64 @@ while (palabrasAmostrar != "ESC") {
 
 alert(cadena); // saludando al cliente
 
-let dinero = parseInt(prompt("Cuanto dinero quieres pasar con la tarjeta?"))
+let cuotas
+let totalApagar
+let decision = prompt("Vas a pagar en efectivo o con tarjeta?")
 
-let cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
+if (decision == "efectivo") {
+    console.log("Pagas el total sin intereses")
+}
 
-let totalApagar 
+if (decision == "tarjeta") {
 
-do{
-    switch(cuotas){
-        case 1:
-            console.log('lo va a pagar en 1 cuotas')
-            totalApagar = dinero + (dinero*0.01)
-            console.log("el total a pagar es de "+totalApagar)
-            cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
-            break;
-        case 2: 
-            console.log('lo va a pagar en 2 cuota')
-            totalApagar = dinero + (dinero*0.03)
-            console.log("el total a pagar es de "+totalApagar)
-            cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
-            break;
-        case 3: 
-            console.log('lo va a pagar en 3 cuota')
-            totalApagar = dinero + (dinero*0.05)
-            console.log("el total a pagar es de "+totalApagar)
-            cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
-            break;
-        case 6: 
-            console.log('lo va a pagar en 6 cuota')
-            totalApagar = dinero + (dinero*0.10)
-            console.log("el total a pagar es de "+totalApagar)
-            cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
-            break;
-        case 12: 
-            console.log('lo va a pagar en 12 cuota')
-            totalApagar = dinero + (dinero*0.20)
-            console.log("el total a pagar es de "+totalApagar)
-            cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
-            break;
-        default:
-            console.log('Ingresaste CUALQUIER COSA, lee bien!')
-            cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
-            break
+    
+    let dinero = parseInt(prompt("Cuanto dinero quieres pasar con la tarjeta?"))
 
-    }
-}while(cuotas != 0)
+    let cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
+    let totalApagar
+    imprimirCuotas(dinero,cuotas,totalApagar)
+
+}
+
+function imprimirCuotas(dinero,cuotas,totalApagar) {
+
+    do {
+        switch (cuotas) {
+            case 1:
+                console.log('lo va a pagar en 1 cuotas')
+                totalApagar = dinero + (dinero * 0.01)
+                console.log("el total a pagar es de " + totalApagar)
+                cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
+                break;
+            case 2:
+                console.log('lo va a pagar en 2 cuota')
+                totalApagar = dinero + (dinero * 0.03)
+                console.log("el total a pagar es de " + totalApagar)
+                cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
+                break;
+            case 3:
+                console.log('lo va a pagar en 3 cuota')
+                totalApagar = dinero + (dinero * 0.05)
+                console.log("el total a pagar es de " + totalApagar)
+                cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
+                break;
+            case 6:
+                console.log('lo va a pagar en 6 cuota')
+                totalApagar = dinero + (dinero * 0.10)
+                console.log("el total a pagar es de " + totalApagar)
+                cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
+                break;
+            case 12:
+                console.log('lo va a pagar en 12 cuota')
+                totalApagar = dinero + (dinero * 0.20)
+                console.log("el total a pagar es de " + totalApagar)
+                cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
+                break;
+            default:
+                console.log('Ingresaste CUALQUIER COSA, lee bien!')
+                cuotas = parseInt(prompt("ingrese la cantidad de cuotas en las cuales quiere pagar ese dinero (1,2,3,6,12) \n selecione (0) para cancelar"))
+                break
+
+        }
+    } while (cuotas != 0)
+}
